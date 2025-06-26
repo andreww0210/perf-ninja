137cef480f9843618953064d2094ffc3d64f00cf
yinghao <itsyinghao@gmail.com>
2025-06-26 08:16:23 +0800

    warmup.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cmake -E rm -rf build
+cmake -E make_directory build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug .. -DCMAKE_C_FLAGS="-g" -DCMAKE_CXX_FLAGS="-g" ..
+cmake --build . --config Debug --parallel 8
+cmake --build . --target validateLab
+cmake --build . --target benchmarkLab
